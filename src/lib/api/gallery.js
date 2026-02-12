a374eea7072cf78182efa815d5851af47dd1d6bb
neelgit1010 <neel.binbrain1010@gmail.com>
2026-02-12 14:41:18 +0530

    update

--- src/lib/api/gallery.js
+++ src/lib/api/gallery.js
@@ -1,5 +1,6 @@
 const API_BASE_URL = "https://dentitydentaladminpanel.vercel.app";
 // const API_BASE_URL = "http://localhost:3000";
+// gallery
 
 export async function fetchGallerys({
   page = 1,
